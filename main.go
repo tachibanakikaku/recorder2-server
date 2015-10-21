8aafcf7d737c1f49f68776710023d4620f8b0ff0
mryoshio <yoshiokaas@gmail.com>
2015-10-21 22:55:28 +0900

    add record model add records method

--- main.go
+++ main.go
@@ -3,12 +3,28 @@ package recorder2
 import (
   "fmt"
   "net/http"
+  "time"
 )
 
 func init() {
-  http.HandleFunc("/", record)
+  http.HandleFunc("/", root)
+  http.HandleFunc("/records", records)
 }
 
-func record(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprint(w, "got request")
+func root(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "please login") // TODO: add login function
+}
+
+func records(w http.ResponseWriter, r *http.Request) {
+  switch r.Method {
+  case "POST": fmt.Fprintf(w, "records posted %s", http.PostForm) // TODO: parse JSON
+  default: http.Error(w, "error occurred", http.StatusInternalServerError) // TODO: use template file
+  }
+}
+
+// TODO: separate into other file
+type Record struct {
+  GroupId int
+  Name string
+  ReceivedAt time.Time
 }
